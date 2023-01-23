cf59bc63a1ec36ffcd5c6c882572b941f1e13652
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2023-01-23 15:14:04 -0700

    Added opencv.js and opencv-react import

--- pages/detection.js
+++ pages/detection.js
@@ -1,7 +1,13 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { OpenCvProvider, useOpenCv } from 'opencv-react';
+import {useEffect} from 'react';
 
 export default function Detection() {
+  const onLoaded = (cv) => {
+    console.log('opencv loaded', cv)
+  }
+
   return (
     <div className={styles.container}>
         <Head >
@@ -14,8 +20,22 @@ export default function Detection() {
                 Detect a go board
                 </h1>
             </main>
+            <OpenCvProvider onLoad={onLoaded} openCvPath='../opencv/opencv.js'>
+              <MyComponent />
+            </OpenCvProvider>
         </Head>
     </div>
     
   )
+}
+
+function MyComponent() {
+  const { loaded, cv } = useOpenCv();
+
+  useEffect(() => {
+    if (cv) {
+    }
+  }, [cv])
+
+  return <p>OpenCv React test</p>
 }
